6557290daf89c4da08422c3c63fa6bf67bfe7db8
DemarioCrawford <Rooneymusic4@gmail.com>
2024-07-25 20:14:52 -0700

    added some comments
    making sure I understand the logic behind the code in English

--- src/StudentServiceApplication.java
+++ src/StudentServiceApplication.java
@@ -8,11 +8,13 @@ public class StudentServiceApplication {
 		
 		
 		try {
+		//list each course 
 		List<Student> students = FileService.readCVS("student-master-list.csv");
 		List<Student> compSciStudents = new ArrayList<>();
 		List<Student> apMathStudents = new ArrayList<>();
 		List<Student> statStudents = new ArrayList<>();
 		
+		//split the students into the correct classes
 		for (Student student: students) {
 			if (student.getCourse().startsWith("COMPSCI")) {
 				compSciStudents.add(student);
@@ -22,11 +24,11 @@ public class StudentServiceApplication {
 				statStudents.add(student);
 			}
 		}
-		
+		//sorting the students in desc order
 		compSciStudents.sort((s1, s2)-> Integer.compare(s2.getGrade(), s1.getGrade()));
 		apMathStudents.sort((s1, s2)-> Integer.compare(s2.getGrade(), s1.getGrade()));
 		statStudents.sort((s1, s2)-> Integer.compare(s2.getGrade(), s1.getGrade()));
-		
+		// write the list to new files
 		FileService.writeCSV("course1.csv", compSciStudents);
 		FileService.writeCSV("course2.csv", apMathStudents);
 		FileService.writeCSV("course3.csv", statStudents);
